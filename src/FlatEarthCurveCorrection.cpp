21ff5f3c39ae60c964f9aed6ecc7abb3e0a07a8c
eeveetza <74705280+eeveetza@users.noreply.github.com>
2020-11-19 11:48:35 +0100

    Update FlatEarthCurveCorrection.cpp
    The modifications include the addition of power series expansion for values of q≤0.1 (as explained in NTIA Report 99-368) and the use of the ACM Algorithm 680 to compute Faddeeva function.

--- src/FlatEarthCurveCorrection.cpp
+++ src/FlatEarthCurveCorrection.cpp
@@ -24,7 +24,7 @@
  |      Returns:  E_gw          - Normalized field strength in mV/m
  |
  *===========================================================================*/
-double FlatEarthCurveCorrection(complex<double> Delta, complex<double> q, double h_1__km, double h_2__km, double d__km, double k)
+double FlatEarthCurveCorrection(complex<double> Delta, complex<double> q, double h_1__km, double h_2__km, double d__km, double k, double a_e__km)
 {
     complex<double> j = complex<double>(0.0, 1.0);
 
@@ -34,14 +34,47 @@ double FlatEarthCurveCorrection(complex<double> Delta, complex<double> q, double
     complex<double> qi = (-0.5 + j * 0.5)*sqrt(k*d__km)*Delta;
     complex<double> p = qi * qi;
 
+    complex<double> p2 = pow(p, 2);
     complex<double> q3 = pow(q, 3);
     complex<double> q6 = pow(q, 6);
+    complex<double> q9 = pow(q, 9);
+ 
+    complex<double> fofx;
+ 
+    if(abs(q) > 0.1){
+        // Find F(p) Eqn (32) NTIA Report 99-368
+        // complex<double> Fofp = 1.0 + sqrt(PI)*j*qi*werf(qi);
+        complex<double> Fofp = 1.0 + sqrt(PI)*j*qi*wofz(qi);
 
-    // Find F(p) Eqn (32) NTIA Report 99-368
-    complex<double> Fofp = 1.0 + sqrt(PI)*j*qi*werf(qi);
+        // Calculate f(x) which is the normalized electric field, E_ratio; Eqn (31) NTIA Report 99-368
+        fofx = Fofp + (1.0 - j * sqrt(PI * p) - (1.0 + 2.0 * p) * Fofp) / (4.0 * q3);
+        fofx = fofx + (1.0 - j * sqrt(PI * p) * (1.0 - p) - 2.0 * p + 5.0 * p2 / 6.0 + (p2 / 2.0 - 1.0) * Fofp) / (4.0 * q6);
+    } else {
+     
+        complex<double> A[10];
+ 
+        A[0] = 1.0;
+        A[1] = -j * sqrt(PI);
+        A[2] = -2.0;
+        A[3] = j * sqrt(PI) * (1.0 + 1.0/(4.0*q3));
+        A[4] = 4.0/3.0 * ( 1.0 + 1.0/(2.0*q3) );
+        A[5] = -j * sqrt(PI) / 4.0 * (1.0 + 3.0 / (4.0 * q3));
+        A[6] = -8.0/15.0 * ( 1.0 + 1.0/q3 + 7.0 / (32.0 * q6) );
+        A[7] = j * sqrt(PI) / 6.0 * ( 1.0 + 5.0/(4.0*q3) + 27.0 /( 32.0*q6 ) );
+        A[8] = 16.0/105.0 * ( 1.0 + 3.0/(2.0*q3) + 27.0/(32.0*q6) );
+        A[9] = -j * sqrt(PI)/24.0 * (1.0 + 7.0/ (4.0*q3) + 5.0/(4.0*q6) + 21.0/(64.0*q9));
+ 
+        double x = d__km / a_e__km * pow(k * a_e__km/2.0, (1.0/3.0));
+         
+        fofx = 0.0 + j*0.0;
+ 
+        for (int ii = 0; ii<10; ii++) {
+            fofx = fofx + A[ii] * pow( exp(j * PI/4.0) * q * pow(x,1.0/2.0), ii);
+        }
+        
+        
+    }
 
-    // Calculate f(x) which is the normalized electric field, E_ratio; Eqn (31) NTIA Report 99-368
-    complex<double> fofx = Fofp + (1.0 - j * sqrt(PI * p) - (1.0 + 2.0 * p) * Fofp) / (4.0 * q3) + (1.0 - j * sqrt(PI * p) * (1.0 - p) - 2.0 * p + 5.0 * p * p / 6.0 + (p * p / 2.0 - 1.0) * Fofp) / (4.0 * q6);
 
     // Now find the final normalized field strength from f(x) and the height-gain function for each antenna
     // A height-gain function for an antenna is expressed as two terms of a Taylor series 
