7392ca43981e12fee12ecf895690a4169cfc656c
Anthony Romaniello <66272872+aromanielloNTIA@users.noreply.github.com>
2024-10-31 13:00:05 -0400

    template wrapper error handling functions

--- src/ReturnCodes.cpp
+++ src/ReturnCodes.cpp
@@ -4,11 +4,21 @@
 
 #include "ITS.TODO-TEMPLATE/ReturnCodes.h"
 
+#ifdef _WIN32
+    // Ensure strcpy_s is available on Windows
+    #ifndef __STDC_LIB_EXT1__
+        #define __STDC_LIB_EXT1__
+    #endif
+    #ifndef __STDC_WANT_LIB_EXT1__
+        #define __STDC_WANT_LIB_EXT1__ 1
+    #endif
+#endif
+#include <cstring>        // for strcpy_s
 #include <string>         // for std::string
 #include <unordered_map>  // for std::unordered_map
 
 namespace ITS {
-// TODO-TEMPLATE: put this function in this software's namespace
+// TODO-TEMPLATE: put these functions in this software's namespace
 
 /*******************************************************************************
  * Get an error message string from a return code.
@@ -36,6 +46,28 @@ std::string GetReturnStatus(int code) {
         msg += "Undefined return code";
     }
     return msg;
+}
+
+/*******************************************************************************
+ * Get an error message string (as C-style string) from a return code.
+ * 
+ * @param[in] code  Integer return code.
+ * @return          A status message corresponding to the input code.
+ ******************************************************************************/
+char *GetReturnStatusCharArray(const int code) {
+    std::string msg = GetReturnStatus(code);
+    char *c_msg = new char[msg.size() + 1];
+    strcpy_s(c_msg, msg.size() + 1, msg.c_str());
+    return c_msg;
+}
+
+/*******************************************************************************
+ * Free the memory allocated by GetReturnStatusCharArray
+ * 
+ * @param[in] c_msg  The status message C-style string to delete
+ ******************************************************************************/
+void FreeReturnStatusCharArray(char *c_msg) {
+    delete[] c_msg;
 }
 
 }  // namespace ITS
