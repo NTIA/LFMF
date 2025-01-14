2b7fad7a0d8f12cb6f16ceadb68da15ff87177a7
Anthony Romaniello <66272872+aromanielloNTIA@users.noreply.github.com>
2025-01-14 13:22:31 -0500

    clang-format

--- include/LFMF.h
+++ include/LFMF.h
@@ -50,16 +50,18 @@ enum SolutionMethod {
  * @see ITS::Propagation::LFMF::WiRoot
  * @see ITS::Propagation::LFMF::AiryFunctionScaling
  ******************************************************************************/
+// clang-format off
 enum AiryFunctionKind {
     AIRY = 1, /**< Airy function of the first kind, @f$ \mathrm{Ai}(x) @f$ */
     AIRYD,    /**< Derivative of `AIRY`, @f$ \mathrm{Ai}'(x) @f$ */
     BAIRY,    /**< Airy function of the second kind, @f$ \mathrm{Bi}(x) @f$ */
     BAIRYD,   /**< Derivative of `BAIRY`, @f$ \mathrm{Bi}'(x) @f$ */
-    WTWO, /**< Hufford's Airy function of a third kind, @f$ \mathrm{Wi}^{(2)} @f$ (Wait's @f$ w_1 @f$) */
-    DWTWO, /**< Derivative of `WTWO`, @f$ \mathrm{Wi}'^{(2)} @f$ (Wait's @f$ w_1' @f$) */
-    WONE, /**< Hufford's Airy function of a third kind, @f$ \mathrm{Wi}'^{(1)} @f$ (Wait's @f$ w_2 @f$)*/
-    DWONE, /**< Derivative of `WONE`, @f$ \mathrm{Wi}'^{(1)} @f$ (Wait's @f$ w_2' @f$) */
+    WTWO,     /**< Hufford's Airy function of a third kind, @f$ \mathrm{Wi}^{(2)} @f$ (Wait's @f$ w_1 @f$) */
+    DWTWO,    /**< Derivative of `WTWO`, @f$ \mathrm{Wi}'^{(2)} @f$ (Wait's @f$ w_1' @f$) */
+    WONE,     /**< Hufford's Airy function of a third kind, @f$ \mathrm{Wi}'^{(1)} @f$ (Wait's @f$ w_2 @f$)*/
+    DWONE,    /**< Derivative of `WONE`, @f$ \mathrm{Wi}'^{(1)} @f$ (Wait's @f$ w_2' @f$) */
 };
+// clang-format on
 
 /*******************************************************************************
  * Multiplicative scaling to use when dealing with Airy functions.
@@ -95,36 +97,47 @@ enum ReturnCode {
     ERROR__SIGMA,                       /**< Sigma is out of range */
     ERROR__POLARIZATION,                /**< Invalid value for polarization */
 };
+// clang-format on
 
 ////////////////////////////////////////////////////////////////////////////////
 // Constants
+// clang-format off
+constexpr double PI = 3.1415926535897932384;   /**< Approximate value of @f$ \pi @f$ */
+constexpr double epsilon_0 = 8.854187817e-12;  /**< Vacuum permittivity (F/m) */
+constexpr double a_0__km = 6370;               /**< Earth radius, in km */
+constexpr double C = 299792458.0;              /**< Speed of light (m/s) */
+constexpr double ETA = 119.9169832 * PI;       /**< Intrinsic impedance of free space (ohms) */
 
-constexpr double PI = 3.1415926535897932384;        /**< Approximate value of @f$ \pi @f$ */
-constexpr double epsilon_0 = 8.854187817e-12;       /**< Vacuum permittivity (F/m) */
-constexpr double a_0__km = 6370;                    /**< Earth radius, in km */
-constexpr double C = 299792458.0;                   /**< Speed of light (m/s) */
-constexpr double ETA = 119.9169832 * PI;            /**< Intrinsic impedance of free space (ohms) */
-
-constexpr int YES = 1;                              /**< Find the derivative i.e., Ai'() or Bi'() */
-constexpr int NO = 0;                               /**< Find Ai() or Bi() */
+constexpr int YES = 1;                         /**< Find the derivative i.e., Ai'() or Bi'() */
+constexpr int NO = 0;                          /**< Find Ai() or Bi() */
+// clang-format on
 
 ////////////////////////////////////////////////////////////////////////////////
 // Data Structures
 
 /** Structure to hold LF/MF model outputs. */
-struct Result
-{
-    double A_btl__db;               /**< Basic transmission loss, in dB */
-    double E_dBuVm;                 /**< Electric field strength, in dB(uV/m) */
-    double P_rx__dbm;               /**< Received power, in dBm */
-    SolutionMethod method;          /**< Solution method: 0 = Flat earth with curve correction, 1 = Residue series */
+struct Result {
+        double A_btl__db;      /**< Basic transmission loss, in dB */
+        double E_dBuVm;        /**< Electric field strength, in dB(uV/m) */
+        double P_rx__dbm;      /**< Received power, in dBm */
+        SolutionMethod method; /**< Method used to obtain results */
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // Public Functions
 
-DLLEXPORT ReturnCode LFMF(const double h_tx__meter, const double h_rx__meter, const double f__mhz, const double P_tx__watt, const double N_s,
-    const double d__km, const double epsilon, const double sigma, const int pol, Result *result);
+DLLEXPORT ReturnCode LFMF(
+    const double h_tx__meter,
+    const double h_rx__meter,
+    const double f__mhz,
+    const double P_tx__watt,
+    const double N_s,
+    const double d__km,
+    const double epsilon,
+    const double sigma,
+    const int pol,
+    Result *result
+);
 
 DLLEXPORT char *GetReturnStatusCharArray(const int code);
 DLLEXPORT void FreeReturnStatusCharArray(char *c_msg);
@@ -132,19 +145,64 @@ DLLEXPORT void FreeReturnStatusCharArray(char *c_msg);
 ////////////////////////////////////////////////////////////////////////////////
 // Private Functions
 
-ReturnCode LFMF_CPP(const double h_tx__meter, const double h_rx__meter, const double f__mhz, const double P_tx__watt, const double N_s,
-    const double d__km, const double epsilon, const double sigma, const Polarization pol, Result *result);
+ReturnCode LFMF_CPP(
+    const double h_tx__meter,
+    const double h_rx__meter,
+    const double f__mhz,
+    const double P_tx__watt,
+    const double N_s,
+    const double d__km,
+    const double epsilon,
+    const double sigma,
+    const Polarization pol,
+    Result *result
+);
 std::string GetReturnStatus(const int code);
-double FlatEarthCurveCorrection(const std::complex<double> delta, const std::complex<double> q, const double h_1__km, const double h_2__km, const double d, const double k, const double a_e__km);
-double ResidueSeries(const double k, const double h_1__km, const double h_2__km, const double nu, const double theta, const std::complex<double> q);
+double FlatEarthCurveCorrection(
+    const std::complex<double> delta,
+    const std::complex<double> q,
+    const double h_1__km,
+    const double h_2__km,
+    const double d,
+    const double k,
+    const double a_e__km
+);
+double ResidueSeries(
+    const double k,
+    const double h_1__km,
+    const double h_2__km,
+    const double nu,
+    const double theta,
+    const std::complex<double> q
+);
 std::complex<double> wofz(const std::complex<double> z);
-std::complex<double> Airy(const std::complex<double> Z, const AiryFunctionKind kind, const AiryFunctionScaling scaling);
-std::complex<double> WiRoot(const int i, std::complex<double> &DWi, const std::complex<double> q, std::complex<double> &Wi,
-    const AiryFunctionKind kind, const AiryFunctionScaling scaling);
-ReturnCode ValidateInput(const double h_tx__meter, const double h_rx__meter, const double f__mhz, const double P_tx__watt,
-    const double N_s, const double d__km, const double epsilon, const double sigma);
+std::complex<double> Airy(
+    const std::complex<double> Z,
+    const AiryFunctionKind kind,
+    const AiryFunctionScaling scaling
+);
+std::complex<double> WiRoot(
+    const int i,
+    std::complex<double> &DWi,
+    const std::complex<double> q,
+    std::complex<double> &Wi,
+    const AiryFunctionKind kind,
+    const AiryFunctionScaling scaling
+);
+ReturnCode ValidateInput(
+    const double h_tx__meter,
+    const double h_rx__meter,
+    const double f__mhz,
+    const double P_tx__watt,
+    const double N_s,
+    const double d__km,
+    const double epsilon,
+    const double sigma
+);
 ReturnCode ValidatePolarization(const int pol);
-bool AlmostEqualRelative(const double A, const double B, const double maxRelDiff = DBL_EPSILON);
+bool AlmostEqualRelative(
+    const double A, const double B, const double maxRelDiff = DBL_EPSILON
+);
 
 }  // namespace LFMF
 }  // namespace Propagation
