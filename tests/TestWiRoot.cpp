ae653ae72f11dec61cda9797523bffca8db0390f
Anthony Romaniello <66272872+aromanielloNTIA@users.noreply.github.com>
2025-01-15 17:52:22 -0500

    Loosen tolerances for cross-platform testing

--- tests/TestWiRoot.cpp
+++ tests/TestWiRoot.cpp
@@ -30,13 +30,13 @@ class TestWiRoot: public ::testing::Test {
 TEST_F(TestWiRoot, AiryKindWONE) {
     // Test for scaling = HUFFORD
     root = WiRoot(i, DWi, q, Wi, kind, scaling);
-    EXPECT_DOUBLE_EQ(root.real(), 1.4829223351194638);
-    EXPECT_DOUBLE_EQ(root.imag(), 1.2843356310540217);
+    EXPECT_NEAR(root.real(), 1.4829223351194638, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 1.2843356310540217, ABSTOL_DBL);
 
     // Test for scaling = WAIT
     root = WiRoot(i, DWi, q, Wi, kind, AiryScaling::WAIT);
-    EXPECT_DOUBLE_EQ(root.real(), 0.22646319357739417);
-    EXPECT_DOUBLE_EQ(root.imag(), 1.7542471947545168);
+    EXPECT_NEAR(root.real(), 0.22646319357739417, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 1.7542471947545168, ABSTOL_DBL);
 }
 
 /** Valid test case: AiryKind::WTWO */
@@ -44,13 +44,13 @@ TEST_F(TestWiRoot, AiryKindWTWO) {
     kind = AiryKind::WTWO;
     // Test for scaling = HUFFORD
     root = WiRoot(i, DWi, q, Wi, kind, scaling);
-    EXPECT_DOUBLE_EQ(root.real(), 0.22646319357739642);
-    EXPECT_DOUBLE_EQ(root.imag(), 1.7542471947545142);
+    EXPECT_NEAR(root.real(), 0.22646319357739642, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 1.7542471947545142, ABSTOL_DBL);
 
     // Test for scaling = WAIT
     root = WiRoot(i, DWi, q, Wi, kind, AiryScaling::WAIT);
-    EXPECT_DOUBLE_EQ(root.real(), 1.482922335119464);
-    EXPECT_DOUBLE_EQ(root.imag(), 1.2843356310540215);
+    EXPECT_NEAR(root.real(), 1.482922335119464, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 1.2843356310540215, ABSTOL_DBL);
 }
 
 /** Test a set of conditions that run for small q and small i */
@@ -59,8 +59,8 @@ TEST_F(TestWiRoot, SmallQSmallI) {
     q = {1.0, 1.0};
     i = 1;
     root = WiRoot(i, DWi, q, Wi, kind, scaling);
-    EXPECT_DOUBLE_EQ(root.real(), 1.4829223351194638);
-    EXPECT_DOUBLE_EQ(root.imag(), 1.2843356310540217);
+    EXPECT_NEAR(root.real(), 1.4829223351194638, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 1.2843356310540217, ABSTOL_DBL);
 }
 
 /** Test a set of conditions that run for small q and large i */
@@ -69,8 +69,8 @@ TEST_F(TestWiRoot, SmallQLargeI) {
     q = {1.0, 1.0};
     i = 11;
     root = WiRoot(i, DWi, q, Wi, kind, scaling);
-    EXPECT_DOUBLE_EQ(root.real(), 6.7395876387056539);
-    EXPECT_DOUBLE_EQ(root.imag(), 11.460104740476151);
+    EXPECT_NEAR(root.real(), 6.7395876387056539, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 11.460104740476151, ABSTOL_DBL);
 }
 
 /** Test a set of conditions that run for large q and small i */
@@ -79,8 +79,8 @@ TEST_F(TestWiRoot, LargeQSmallI) {
     q = {5.0, 5.0};
     i = 1;
     root = WiRoot(i, DWi, q, Wi, kind, scaling);
-    EXPECT_DOUBLE_EQ(root.real(), 1.2695003468987784);
-    EXPECT_DOUBLE_EQ(root.imag(), 1.9226740699637952);
+    EXPECT_NEAR(root.real(), 1.2695003468987784, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 1.9226740699637952, ABSTOL_DBL);
 }
 /** Test a set of conditions that run for large q and large i */
 TEST_F(TestWiRoot, LargeQLargeI) {
@@ -88,8 +88,8 @@ TEST_F(TestWiRoot, LargeQLargeI) {
     q = {5.0, 5.0};
     i = 11;
     root = WiRoot(i, DWi, q, Wi, kind, scaling);
-    EXPECT_DOUBLE_EQ(root.real(), 6.9479928400612074);
-    EXPECT_DOUBLE_EQ(root.imag(), 11.742393555292688);
+    EXPECT_NEAR(root.real(), 6.9479928400612074, ABSTOL_DBL);
+    EXPECT_NEAR(root.imag(), 11.742393555292688, ABSTOL_DBL);
 }
 
 /** WiRoot should throw an exception when `i` is <= 0 */
