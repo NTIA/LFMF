9bea014aedda017c40fceda5908d2cfbab67078d
Chen Heroy <cheroy.ctr@ntia.gov>
2026-06-17 13:34:26 -0600

    FreeReturnStatusCharArray

--- src/ReturnCodes.cpp
+++ src/ReturnCodes.cpp
@@ -100,7 +100,7 @@ char *GetReturnStatusCharArray(const int code) {
  * 
  * @param[in] c_msg  The status message C-style string to delete
  ******************************************************************************/
-void FreeCharArray(char *c_msg) {
+void FreeReturnStatusCharArray(char *c_msg) {
     delete[] c_msg;
 }
 
